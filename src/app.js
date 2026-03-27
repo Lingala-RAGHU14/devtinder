cc8d9731c3acfc83627d4340090588538874053d
Lingala-RAGHU14 <raghulingala4@gmail.com>
2026-03-27 23:00:28 +0530

    routes & middlewares

--- src/app.js
+++ src/app.js
@@ -1,24 +1,72 @@
 const express = require("express")
+const {adminAuth,userAuth} = require("./middlewares/auth.js")
 
-const app = express()
+const app = express() 
 
-app.get("/users", (req,res)=> {
-    res.send({
-        name : "raghu",
-        fatherName : "Rajesh",
-        BrotherName: "Rohith"
-    })
+// when we "use" Handle Auth Middlewares for All GET, POST .... requests
+// "use" === "all" (both are one in same)
+
+
+app.use("/admin",adminAuth )
+
+app.get("/user/login", (req,res)=> {
+    res.send("user logged in successfully")
 })
 
-app.post("/users", (req,res) => {
-    res.send("data successfully saved to Database")
+app.get("/user",userAuth,(req,res)=> {
+    res.send("user data sent succeeded")
 })
-app.delete("/users", (req,res) => {
-    res.send("data successfully deleted from Database")
+app.get("/admin/getData",(req,res)=>{
+     res.send("The data is successfully sent")
+
 })
-app.patch("/users", (req,res) => {
-    res.send("data successfully patched from Database")
+
+app.get("/admin/deleteUser",(req,res)=> {
+     res.send("user deleted successfully")
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.use("/router", [rH1,rH2,rH3,rH4,rH5]) // writing routes inside the array nothing will break 
+// app.use("/users", (req,res,next) => {
+//     console.log("this is 1st call")
+//     // res.send("Response 1")
+//     next()
+// },(req,res)=>{
+//      console.log("this is 2nd call")
+//     res.send("response 2")
+// })
+
+// app.get("/users", (req,res)=> {
+//     res.send({
+//         name : "raghu",
+//         fatherName : "Rajesh",
+//         BrotherName: "Rohith"
+//     })
+// })
+
+// app.delete("/users", (req,res) => {
+//     res.send("data successfully deleted from Database")
+// })
+// app.patch("/users", (req,res) => {
+//     res.send("data successfully patched from Database")
+// })
 
 // this will match all the http methods to api call /test
 app.use("/test", (req,res)=> {   // request handlers && order is very important 
---
+++ src/middlewares/auth.js
@@ -0,0 +1,23 @@
+const adminAuth = (req,res,next)=> {
+    console.log("admin auth getting checked")
+    const token = "xyz"
+    const isAdminAuthorized = token === "xyz"
+    if(!isAdminAuthorized) {
+        res.status(401).send("unauthorized request")
+    }else {
+        next()
+    }
+}
+
+const userAuth = (req,res,next)=> {
+    console.log("admin auth getting checked")
+    const token = "xyz"
+    const isAdminAuthorized = token === "xyz"
+    if(!isAdminAuthorized) {
+        res.status(401).send("unauthorized request")
+    }else {
+        next()
+    }
+}
+
+module.exports = {  adminAuth, userAuth }
