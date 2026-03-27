9014621bab12044b200343b4953bc80d1943146a
Lingala-RAGHU14 <raghulingala4@gmail.com>
2026-03-27 13:02:27 +0530

    req handlers & rotes

--- src/app.js
+++ src/app.js
@@ -1,1 +1,35 @@
-console.log("hello raghu")
+const express = require("express")
+
+const app = express()
+
+app.get("/users", (req,res)=> {
+    res.send({
+        name : "raghu",
+        fatherName : "Rajesh",
+        BrotherName: "Rohith"
+    })
+})
+
+app.post("/users", (req,res) => {
+    res.send("data successfully saved to Database")
+})
+app.delete("/users", (req,res) => {
+    res.send("data successfully deleted from Database")
+})
+app.patch("/users", (req,res) => {
+    res.send("data successfully patched from Database")
+})
+
+// this will match all the http methods to api call /test
+app.use("/test", (req,res)=> {   // request handlers && order is very important 
+    res.send("server testing by routings")
+})
+
+
+// app.use("/helo", (req,res)=> {
+//     res.send("hello hello hello")
+// })
+
+app.listen(3000, () => {
+    console.log("server sucessfully started")
+})
